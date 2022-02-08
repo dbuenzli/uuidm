dee23af6df70d1f2c6b5961eaae7b105bc6ab0ef
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-02-08 16:01:22 +0100

    Remove some watermarks.

--- src/uuidm.ml
+++ src/uuidm.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2008 The uuidm programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let md5 = Digest.string
