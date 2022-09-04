e8258098fd5d3b2b7b9861a80f06a24b0b3b9d93
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-09-04 22:57:45 +0200

    Track b0

--- B0.ml
+++ B0.ml
@@ -1,17 +1,13 @@
 open B0_kit.V000
-open B00_std
 
 (* OCaml library names *)
 
-let uuidm = B0_ocaml.libname "uuidm"
 let cmdliner = B0_ocaml.libname "cmdliner"
+let uuidm = B0_ocaml.libname "uuidm"
 
 (* Libraries *)
 
-let uuidm_lib =
-  let srcs = Fpath.[`Dir (v "src")] in
-  let requires = [] in
-  B0_ocaml.lib uuidm ~doc:"The uuidm library" ~srcs ~requires
+let uuidm_lib = B0_ocaml.lib uuidm ~srcs:[`Dir ~/"src"]
 
 (* Tests *)
 
@@ -20,45 +16,42 @@ let test_exe src ~doc =
   let srcs = Fpath.[`File src] in
   let meta = B0_meta.(empty |> tag test) in
   let requires = [ uuidm ] in
-  B0_ocaml.exe (Fpath.basename ~no_ext:true src) ~srcs ~doc ~meta ~requires
+  B0_ocaml.exe (Fpath.basename ~strip_ext:true src) ~srcs ~doc ~meta ~requires
 
 let test = test_exe "test/test.ml" ~doc:"Test suite"
 let perf = test_exe "test/perf.ml" ~doc:"Test performance"
 
 let uuidtrip =
   let doc = "Generates universally unique identifiers (UUIDs)" in
-  let srcs = Fpath.[`File (v "test/uuidtrip.ml")] in
+  let srcs = [`File ~/"test/uuidtrip.ml"] in
   let requires = [uuidm; cmdliner] in
-  B0_ocaml.exe "uuidtrip" ~doc ~srcs ~requires
+  B0_ocaml.exe "uuidtrip" ~public:true ~doc ~srcs ~requires
 
 (* Packs *)
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> tag B0_opam.tag
-    |> add authors ["The uuidm programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/uuidm"
-    |> add online_doc "https://erratique.ch/software/uuidm/doc/"
-    |> add licenses ["ISC"]
-    |> add repo "git+https://erratique.ch/repos/uuidm.git"
-    |> add issues "https://github.com/dbuenzli/uuidm/issues"
-    |> add description_tags
-      ["uuid"; "codec"; "org:erratique"]
-    |> add B0_opam.Meta.depopts ["cmdliner", ""]
-    |> add B0_opam.Meta.conflicts
-      [ "cmdliner", {|< "1.1.0"|}]
-    |> add B0_opam.Meta.depends
+    B0_meta.empty
+    |> ~~ B0_meta.authors ["The uuidm programmers"]
+    |> ~~ B0_meta.maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> ~~ B0_meta.homepage "https://erratique.ch/software/uuidm"
+    |> ~~ B0_meta.online_doc "https://erratique.ch/software/uuidm/doc/"
+    |> ~~ B0_meta.licenses ["ISC"]
+    |> ~~ B0_meta.repo "git+https://erratique.ch/repos/uuidm.git"
+    |> ~~ B0_meta.issues "https://github.com/dbuenzli/uuidm/issues"
+    |> ~~ B0_meta.description_tags ["uuid"; "codec"; "org:erratique"]
+    |> B0_meta.tag B0_opam.tag
+    |> ~~ B0_opam.depopts ["cmdliner", ""]
+    |> ~~ B0_opam.conflicts [ "cmdliner", {|< "1.1.0"|}]
+    |> ~~ B0_opam.depends
       [ "ocaml", {|>= "4.08.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
         "topkg", {|build & >= "1.0.3"|};
       ]
-    |> add B0_opam.Meta.build
+    |> B0_meta.add B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"
           "--with-cmdliner" "%{cmdliner:installed}%"]]|}
   in
-  B0_pack.v "default" ~doc:"uuidm package" ~meta ~locked:true @@
+  B0_pack.make "default" ~doc:"uuidm package" ~meta ~locked:true @@
   B0_unit.list ()
