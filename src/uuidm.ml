cce40531851a03ba267fbc178746ab08342c6cd8
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-09-12 22:05:25 +0200

    Spdxify.

--- src/uuidm.ml
+++ src/uuidm.ml
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2008 The uuidm programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 let md5 = Digest.string
@@ -278,19 +278,3 @@ let to_string ?(upper = false) u =
 let pp ppf u = Format.pp_print_string ppf (to_string u)
 let pp_string ?upper ppf u = Format.pp_print_string ppf (to_string ?upper u)
 let print = pp_string (* deprecated *)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2008 The uuidm programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
